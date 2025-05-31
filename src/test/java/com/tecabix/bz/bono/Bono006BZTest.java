e1e8d8687d2be42134f27b024b80b7969c21a962
Angel Abinadi Ramirez Urrutia <abinadi.ramirez.urrutia@tecabix.com>
2025-05-31 10:54:26 -0600

    init

--- src/test/java/com/tecabix/bz/bono/Bono006BZTest.java
+++ src/test/java/com/tecabix/bz/bono/Bono006BZTest.java
@@ -3,7 +3,6 @@ package com.tecabix.bz.bono;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
